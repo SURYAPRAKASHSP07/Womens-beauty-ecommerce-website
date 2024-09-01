7a3d3e712b0161eec2266e4ff9d17fb1ac9162f0
Surya Prakash S P <151605828+SURYAPRAKASHSP07@users.noreply.github.com>
2024-09-01 06:00:57 -1200

    Update new.js

--- new.js
+++ new.js
@@ -12,6 +12,7 @@ navContainer.addEventListener("click", function () {
    ul.classList.toggle("active");
 });
 
+
 // blusher details adding function
 const productContainer = document.querySelector("#productContainer");
 
@@ -23,43 +24,36 @@ async function fetchBlushdata() {
       output += `
         <div class="card">
         <i class="favHeart fa-regular fa-heart " data-id="${product.id}"></i>
+
          <span class="discountPercentage">${product.discount_percentage}</span>
+
          <img src="${product.image_link}" alt="${product.name}">
+
          <div class="card-body">
+
             <p class="productBrand" style="text-transform: uppercase;font-weight: bold;margin-top:-15px;">${product.brand}</p>
 
             <p id="productName" class="productName text-primary font-monospace fs-6" style="margin-top:-15px">${product.name}</p>
 
            <div class="price" style="margin-top: -20px;">
-            <span class="productPrice fs-4 text-secondary me-2 fs-4">$. ${product.price}</span>
-            
-            <span class="actualPrice fs-5 text-danger text-decoration-line-through"> $.${product.actual_price}</span>
-           </div>
+            <span class="productPrice fs-6 text-secondary me-2 fs-4">$. ${product.price}</span>
 
-         </div>
-         <div class="btns" style="margin-top:-10px;">
-            <button>Buy now</button>
-            <button>Add to cart</button>
+            <span class="actualPrice fs-6 text-danger text-decoration-line-through"> $.${product.actual_price}</span>
+
+            </div>
+            <button class="cartBtn">Add to cart</button>
+
          </div>
       </div>`;
    });
    productContainer.innerHTML = output;
-
 }
 fetchBlushdata();
 
-// add to Favourite
-const favHearts = document.querySelectorAll(".favHeart");
 
-favHearts.forEach((heart) => {
-   heart.addEventListener("click", () => {
-      heart.classList.toggle("added");
-   });
-});
 
 // bronzer details adding function
 const bronzerCards = document.querySelector("#bronzerCards");
-
 async function bronzerCardsData() {
    const res = await fetch("Bronzer.json");
    const data = await res.json();
@@ -69,7 +63,7 @@ async function bronzerCardsData() {
       <div class="card">
         <i class="favHeart fa-regular fa-heart " data-id="${product.id}"></i>
          <span class="discountPercentage">30% OFF</span>
-         <img src="${product.api_featured_image}" alt="${product.name} ">
+         <img src="${product.image_link}" alt="${product.name} ">
          <div class="card-body">
 
             <p class="productBrand" style="text-transform: uppercase;font-weight: bold;margin-top:-10px;">${product.brand} </p>
@@ -83,16 +77,43 @@ async function bronzerCardsData() {
 
                <span class="actualPrice fs-5 text-danger text-decoration-line-through">$. 25.00</span>
             </div>
-
-         </div>
-         <div class="btns" style="margin-top:-10px;">
-            <button>Buy now</button>
-            <button>Add to cart</button>
+            <button class="cartBtn">Add to cart</button>
          </div>
       </div>`
    });
    bronzerCards.innerHTML = output;
+}
+bronzerCardsData();
 
+
+// eyeliner details adding function
+const eyelinerContainer = document.querySelector("#eyelinerContainer");
+async function eyelinerFetch() {
+   const res = await fetch("Eyeliner.json");
+   const data = await res.json();
+   let output = "";
+   data.forEach((product) => {
+      output += `
+        <div class="card">
+        <i class="favHeart fa-regular fa-heart " data-id="${product.id}"></i>
+         <span class="discountPercentage">35% OFF</span>
+         <img src="${product.image_link}" alt="${product.name}">
+         <div class="card-body">
+            <p class="productBrand" style="text-transform: uppercase;font-weight: bold;margin-top:-15px;">${product.brand}</p>
+
+            <p id="productName" class="productName text-primary font-monospace fs-6" style="margin-top:-15px">${product.name}</p>
+
+           <div class="price" style="margin-top: -20px;">
+            <span class="productPrice fs-4 text-secondary me-2 fs-4">$. ${product.price}</span>
+            <span class="actualPrice fs-5 text-danger text-decoration-line-through"> $. 15.0</span>
+           </div>
+           <button class="cartBtn">Add to cart</button>
+         </div>
+      </div>`;
+   });
+   eyelinerContainer.innerHTML = output;
+
+   // add to Favourite
    const favHearts = document.querySelectorAll(".favHeart");
 
    favHearts.forEach((heart) => {
@@ -101,11 +122,16 @@ async function bronzerCardsData() {
          console.log(e.target.dataset.id);
       });
    });
+
+   // Add to cart button function
+   const cartBtn = document.querySelectorAll(".cartBtn");
+
+   cartBtn.forEach((btn) => {
+      btn.addEventListener("click", (e) => {
+      });
+   });
 }
-bronzerCardsData();
-
-
-
+eyelinerFetch();
 
 
 
